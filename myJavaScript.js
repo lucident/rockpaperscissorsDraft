2bf8c89fe7e6d0b419386fa7cb992f8d54f4a661
lucident <148405218+lucident@users.noreply.github.com>
2024-02-22 12:43:58 -0500

    Add files via upload

---
+++ myJavaScript.js
@@ -0,0 +1,11 @@
+function myComputerChoice(params) {
+    let chicken = Math.floor(Math.random() * 3)
+    if (chicken = 2) {
+        chicken = "Rock"
+    } else if (chicken = 1){
+        chicken = "Scissors"
+    } else {
+        chicken = "Paper"
+    }
+    return chicken
+}
